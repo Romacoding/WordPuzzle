7b5dbe1b54f900e69a7e7b321ad1ae9048f6a0f8
Roman Ostash <roman.ostash@stots.edu>
2018-09-15 11:52:19 -0400

    added DOM part of the game for better interaction with a user

--- word-puzzle.js
+++ word-puzzle.js
@@ -2,6 +2,19 @@ const WordPuzzle = function (word, remainingGuesses) {
     this.word = word.toLowerCase().split('')
     this.remainingGuesses = remainingGuesses
     this.guessedLetters = []
+    this.status = 'playing'
+}
+
+WordPuzzle.prototype.calculateStatus = function () {
+    const finished = this.word.every((letter) => this.guessedLetters.includes(letter))
+
+    if (this.remainingGuesses === 0) {
+        this.status = 'failed'
+    } else if (finished) {
+        this.status = 'finished'
+    } else {
+        this.status = 'playing'
+    }
 }
 
 WordPuzzle.prototype.getPuzzle = function () {
@@ -30,16 +43,6 @@ WordPuzzle.prototype.makeGuess = function (guess) {
     if (isUnique && isBadGuess) {
         this.remainingGuesses--
     }
+
+    this.calculateStatus()
 }
-
-const game1 = new WordPuzzle('Cat', 2)
-
-console.log(game1.getPuzzle())
-console.log(game1.remainingGuesses)
-
-window.addEventListener('keypress', function (e) {
-    const guess = String.fromCharCode(e.charCode)
-    game1.makeGuess(guess)
-    console.log(game1.getPuzzle())
-    console.log(game1.remainingGuesses)
-})
