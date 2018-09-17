361cb00bb9992938902993a2ba9c73d118a90c7c
Roman Ostash <roman.ostash@stots.edu>
2018-09-17 19:01:40 -0400

    implemented custom getters

--- word-puzzle.js
+++ word-puzzle.js
@@ -16,7 +16,7 @@ class WordPuzzle {
             this.status = 'playing'
         }
     }
-    getStatusMessage() {
+    get statusMessage() {
         if (this.status === 'playing') {
             return `Guesses left: ${this.remainingGuesses}`
         } else if (this.status === 'failed') {
@@ -25,7 +25,7 @@ class WordPuzzle {
             return 'Great work! You guessed the work.'
         }
     }
-    getPuzzle() {
+    get puzzle() {
         let puzzle = ''
 
         this.word.forEach((letter) => {
